ee02930dd0a1419dd5646ddfa2aae521a5716d66
Adnan sora <165552470+adnansora04@users.noreply.github.com>
2025-07-02 16:35:00 +0530

    Add files via upload

--- LED001/index.js
+++ LED001/index.js
@@ -1,28 +1,14 @@
-document.body.classList.add('cpl-001')
+if (window.location.href === "https://www.ledlightingsupply.com/photometric-plan") {
 
-const titles = document.querySelectorAll('.form-title');
-titles.forEach(title => {
-  if (title.textContent.includes('Get Your Custom Lighting Plan')) {
-    title.style.display = 'none';
-  }
-});
-
-
-document.querySelectorAll('plan-layout > plan-left-column > plan-form > form-subtitle').forEach(el => {
-  el.style.display = 'none';
-});
-
+document.body.classList.add('cpl-001');
+document.querySelector('.form-title').classList.add('hide-this-title');
+document.querySelector('plan-layout > plan-left-column > plan-form > form-subtitle')?.classList.add('hide-this-subtitle');
+document.querySelector('.plan-layout .plan-right-column .plan-content span')?.classList.add('hide-this-span');
 
 const title= document.querySelector(".plan-title")
 console.log(title);
 title.innerHTML="Custom Lighting Plan with free Consultation"
 
-
-
-const span = document.querySelector('.plan-layout .plan-right-column .plan-content span');
-if (span) {
-  span.style.display = 'none'; // Hides the span element
-}
 
 const targets = document.querySelectorAll('.plan-layout > .plan-right-column > .plan-content ');
 
@@ -42,3 +28,60 @@ targets.forEach(target => {
   
   target.insertAdjacentHTML('afterend', newHTML);
 });
+}
+
+
+
+
+// fan 
+
+if (window.location.href === "https://www.ledlightingsupply.com/ceiling-fan-layout") {
+document.body.classList.add('cpl-001') 
+const titles = document.querySelectorAll('.subtitle');
+titles.forEach(title => {
+  if (title.textContent.includes('Get Your Custom Fan Plan With Product Recommendations')) {
+    title.style.display = 'none';
+  }
+});
+
+
+// document.querySelectorAll('plan-section-2 > form > subtitle-banner').forEach(el => {
+//   el.style.display = 'none';
+// });
+
+
+const title= document.querySelector(".title")
+console.log(title);
+title.innerHTML="  Custom Fan Plan free Consultation"
+
+document.querySelectorAll('.plans-container .plan-section-2 .form .subtitle-banner').forEach(el => {
+  el.style.display = 'none';
+});
+
+const span = document.querySelector('.plan-description .content span');
+if (span) {
+  span.style.display = 'none'; // Hides the actual <span> element
+}
+
+
+const targets = document.querySelectorAll('.plan-description .content');
+
+targets.forEach(target => {
+  const newHTML = `
+     <div class="fixture-box">
+      <div>
+        <h3>Fixture Count & Recommendations</h3>
+        <p>
+          Our Professionally designed lighting plan comes with recommended products
+          for improved energy efficiency and reduced maintenance.
+        </p>
+      </div>
+      <img src="https://res.cloudinary.com/diilhbcp9/image/upload/v1751356625/advice_1_1_botbrx.png" alt="icon" />
+    </div>
+  `;
+  
+  target.insertAdjacentHTML('afterend', newHTML);
+});
+
+
+}
