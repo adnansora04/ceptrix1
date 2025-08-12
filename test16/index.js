ed2c1e67424c883ba565be9647fcec2dbb843340
Adnan sora <165552470+adnansora04@users.noreply.github.com>
2025-08-12 13:15:58 +0530

    Add files via upload

--- test16/index.js
+++ test16/index.js
@@ -17,7 +17,7 @@ waitForElement('.wijcbf_form_wrapper', (wrapper) => {
             .forEach(el => {
                 if (!el.previousElementSibling?.classList.contains('gmd-label')) {
                     el.insertAdjacentHTML('beforebegin', `
-                        <div class="gmd-label">
+                        <div class="gmd-label contents">
                             <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/Vector_8_sq5tni.png" alt="calendar icon" />
                             <span class="time-text">Kies een dag</span>
                         </div>
@@ -25,12 +25,11 @@ waitForElement('.wijcbf_form_wrapper', (wrapper) => {
                 }
             });
 
-
         document.querySelectorAll('.wijcbf_form_wrapper #wijcbf_date_row')
             .forEach(el => {
                 if (!el.nextElementSibling?.classList.contains('gmd-label')) {
                     el.insertAdjacentHTML('afterend', `
-                        <div class="gmd-label section">
+                        <div class="gmd-label section" style="display:none">
                             <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/mingcute_time-line_qcznp8.png" alt="clock icon" />
                             <span class="time-text">Kies een tijd</span>
                         </div>
@@ -42,39 +41,29 @@ waitForElement('.wijcbf_form_wrapper', (wrapper) => {
     function bindDateClick() {
         const dateEls = document.querySelectorAll('.wijcbf_form_wrapper .wijcbf_date_container .wijcbf_date');
         const slotEls = document.querySelectorAll('.wijcbf_form_wrapper .wijcbf_flex_container .wijcbf_slot_container');
+        const timeLabel = document.querySelector('.gmd-label.section');
 
         dateEls.forEach(dateEl => {
             dateEl.addEventListener('click', () => {
                 dateEls.forEach(el => el.classList.remove('active'));
                 slotEls.forEach(el => el.classList.remove('active'));
+
                 dateEl.classList.add('active');
                 const index = [...dateEls].indexOf(dateEl);
                 if (slotEls[index]) slotEls[index].classList.add('active');
+
+                // Show "Kies een tijd" label when a date is selected
+                if (timeLabel) timeLabel.style.display = 'flex';
             });
         });
     }
 
-    function setDefaultActive() {
-        const dateEls = document.querySelectorAll('.wijcbf_form_wrapper .wijcbf_date_container .wijcbf_date');
-        const slotEls = document.querySelectorAll('.wijcbf_form_wrapper .wijcbf_flex_container .wijcbf_slot_container');
-        if (dateEls.length && !document.querySelector('.wijcbf_date.active')) {
-            dateEls.forEach(el => el.classList.remove('active'));
-            slotEls.forEach(el => el.classList.remove('active'));
-            dateEls[0].classList.add('active');
-            if (slotEls[0]) slotEls[0].classList.add('active');
-        }
-    }
-
-    
     insertLabels();
     bindDateClick();
-    setDefaultActive();
-
 
     const observer = new MutationObserver(() => {
         insertLabels();
         bindDateClick();
-        setDefaultActive();
     });
     observer.observe(wrapper, { childList: true, subtree: true });
 });
