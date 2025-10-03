41ff1f0c383065a4570e32b12d51c0431c437b32
Adnan sora <165552470+adnansora04@users.noreply.github.com>
2025-10-03 13:44:03 +0530

    Add files via upload

---
+++ test24/index.js
@@ -0,0 +1,47 @@
+
+  document.body.classList.add('gmd-001');
+let priceEl = document.querySelector('.totaalregel #custom-regular-price');
+if (priceEl) {
+  priceEl.textContent = priceEl.textContent.replace("€", "").trim();
+}
+
+
+let priceEls = document.querySelector('.totaalregel #custom-kassakorting');
+if (priceEls) {
+  priceEls.textContent = priceEls.textContent.replace("€", "").trim();
+}
+
+
+
+let priceE = document.querySelector('.totaalregel #custom-sale-price');
+if (priceE) {
+  priceE.textContent = priceE.textContent.replace("€", "").trim();
+}
+
+
+
+let price = document.querySelector('.wc-pao-subtotal-line .price span');
+if (price) {
+  price.textContent = price.textContent.replace("€", "").trim();
+}
+
+
+let Els = document.querySelectorAll('.elementor-grid .astra-shop-summary-wrap .price .woocommerce-Price-amount .woocommerce-Price-currencySymbol');
+
+Els.forEach(el => {
+  el.textContent = el.textContent.replace("€", "").trim();
+});
+
+const [first, second] = document.querySelectorAll('span.woocommerce-Price-currencySymbol');
+
+if (second) {
+  second.classList.add('hide-currency');
+}
+
+let pric = document.querySelector('.totaalregel #custom-kassakorting');
+if (pric) {
+  pric.textContent = pric.textContent
+    .replace("€", "")       
+    .replace(/^-/, "")      
+    .trim();
+}
